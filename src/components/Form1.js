d8ddba6515a2c180a2e4f33462bd6b6fc281f97c
vaidehi2401 <dholevaidehi2@gmail.com>
2023-12-08 21:39:44 +0530

    modified css

--- src/components/Form1.js
+++ src/components/Form1.js
@@ -2,29 +2,20 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 import {useEffect, useState} from "react";
 import {useNavigate} from 'react-router-dom'
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
-import PhoneInput from 'react-phone-input-2';
-import 'react-phone-input-2/lib/style.css';
-
+import Students from "./Students";
 
 function Form1() {
     const navigate = useNavigate();
-   const[state, setState] = React.useState({fname:"", lname:"", email:""})
-   const [phoneNumber, setPhoneNumber] = useState('');
-   const handleChange = (value) => {
-    setPhoneNumber(value);
-    console.log()
-  };
+   const[state, setState] = React.useState({fname:"", lname:"", email:"",cc :"+91", pnum:""})
    function reset(ev){
     ev.preventDefault();
-    setState({fname:"", lname:"", email:""})
-    setPhoneNumber('');
+    setState({fname:"", lname:"", email:"",cc :"+91", pnum:""})
+    
    }
   const value = Object.values(state)
-   var s = `Name : ${value[0]} ${value[1]} \nPhone number: ${phoneNumber}\nEmail: ${value[2]}`;
+   var s = `Name : ${value[0]} ${value[1]} \nPhone number:${value[3]} ${value[4]} \nEmail: ${value[2]}`;
  const send=()=>{
     alert(s);
     navigate('home');
@@ -51,17 +42,33 @@ class="form-control" placeholder="First name" aria-label="Username" aria-describ
    onChange ={(ev)=>{setState({...state, lname:ev.target.value});}}
    class="form-control" placeholder="Last name" aria-label="Username" aria-describedby="basic-addon1"></input></div></div>
       <h6>Phone number</h6>
-      <div className='inp1'>
-    <PhoneInput 
-          country={'in'}
-          value={phoneNumber}
-          onChange={handleChange}
-          inputProps={{
-            required: true,
-          }
-        }
-        style={{width: "90%"}}
-        ></PhoneInput></div>
+      
+      <div className='row align-items-start'>
+        <div className='col-1'>
+          
+        <select className = "inp2"
+        value={state.cc}
+        onChange ={(ev)=>{setState({...state, cc:ev.target.value});}}
+        >
+  {
+    Students.countries.map((country)=>(
+      <option value={country.dial_code} key = {country.dial_code}>
+        {country.dial_code}
+      </option>))
+  }
+</select>
+<span class="custom-arrow"></span>
+
+      </div>
+<div className = "col-2">
+<div className='inp3'>
+<input type="text"
+  value={state.pnum}
+  name="fname"  
+  onChange ={(ev)=>{setState({...state, pnum:ev.target.value});
+}}
+class="form-control" placeholder="Phone number" aria-label="Username" aria-describedby="basic-addon1"></input>
+</div></div></div>
       <h6>Email ID</h6>
       <div className = "inp1">
       <div class="input-group mb-3">
@@ -76,6 +83,7 @@ class="form-control" placeholder="First name" aria-label="Username" aria-describ
     </div>
       </form>
       </div>
+      
       </div>
   )
 }
